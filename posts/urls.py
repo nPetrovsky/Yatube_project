9341f7383d3169485119831c73e5bcd801748bfc
nPetrovsky <npetrovsky.3012@yandex.ru>
2024-07-03 16:53:39 +0300

    Add files via upload

---
+++ posts/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+app_name = 'posts'
+
+urlpatterns = [
+    path('', views.index),
+    path('index.html', views.index, name='index'),
+    path('group_list.html', views.group_posts, name='group_posts'),
+]
---
+++ posts/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+
+
+def index(request):
+    template = 'posts/index.html'
+    title = 'Это главная страница проекта Yatube'
+    context = {
+        'title': title,
+    }
+    return render(request, template, context) 
+
+
+def group_posts(request):
+    template = 'posts/group_list.html'
+    title = 'Здесь будет информация о группах проекта Yatube'
+    context = {
+        'title': title,
+    }
+    return render(request, template, context)
